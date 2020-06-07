a7b7e1fb27ffcdde27b7a4b1052bb31ff04238d9
Mehak <mganju2@illinois.edu>
2020-06-07 15:52:59 -0500

    button functions

--- app/src/main/java/com/example/activism_app/MainActivity.java
+++ app/src/main/java/com/example/activism_app/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.activism_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -14,10 +16,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton blm = findViewById(R.id.BLM);
+        blm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openBLM();
+            }
+        });
         ImageButton pride = findViewById(R.id.Pride);
-        ImageButton mfol = findViewById(R.id.MFOL);
-        ImageButton meToo = findViewById(R.id.MeToo);
-        ImageButton global = findViewById(R.id.GlobalW);
+        pride.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openPride();
+            }
+        });
 
+    }
+    public void openBLM() {
+        Intent intent = new Intent(this, BLM.class);
+        startActivity(intent);
+    }
+    public void openPride() {
+        Intent intent = new Intent(this, Pride.class);
+        startActivity(intent);
     }
 }
