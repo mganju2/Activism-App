09e50c1352dc69b18fe3e159036d2e5cc595e0c8
Mehak <mganju2@illinois.edu>
2020-06-06 23:21:29 -0500

    main activity

--- app/src/main/java/com/example/activism_app/MainActivity.java
+++ app/src/main/java/com/example/activism_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.activism_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton blm = findViewById(R.id.BLM);
+        ImageButton pride = findViewById(R.id.Pride);
+        ImageButton mfol = findViewById(R.id.MFOL);
+        ImageButton meToo = findViewById(R.id.MeToo);
+        ImageButton global = findViewById(R.id.GlobalW);
+
     }
 }
