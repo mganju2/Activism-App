26f88b2e6a1e281b846c442055a2cafd1aa490db
Mehak <mganju2@illinois.edu>
2020-06-07 16:13:34 -0500

    new pages

---
+++ app/src/main/java/com/example/activism_app/GlobalWarming.java
@@ -0,0 +1,14 @@
+package com.example.activism_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GlobalWarming extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global_warming);
+    }
+}
--- app/src/main/java/com/example/activism_app/MainActivity.java
+++ app/src/main/java/com/example/activism_app/MainActivity.java
@@ -29,7 +29,27 @@ public class MainActivity extends AppCompatActivity {
                 openPride();
             }
         });
-
+        ImageButton mfol = findViewById(R.id.MFOL);
+        mfol.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openMFOL();
+            }
+        });
+        ImageButton meToo = findViewById(R.id.MeToo);
+        meToo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMeToo();
+            }
+        });
+        ImageButton global = findViewById(R.id.GlobalW);
+        global.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGlobal();
+            }
+        });
     }
     public void openBLM() {
         Intent intent = new Intent(this, BLM.class);
@@ -37,6 +57,18 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openPride() {
         Intent intent = new Intent(this, Pride.class);
+        startActivity(intent);
+    }
+    public void openMFOL() {
+        Intent intent = new Intent(this, MFOL.class);
+        startActivity(intent);
+    }
+    public void openMeToo() {
+        Intent intent = new Intent(this, MeToo.class);
+        startActivity(intent);
+    }
+    public void openGlobal() {
+        Intent intent = new Intent(this, GlobalWarming.class);
         startActivity(intent);
     }
 }
