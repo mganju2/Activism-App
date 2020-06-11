add3ff6d0b5aca19aa73575fae615591c39cec4c
Mehak <mganju2@illinois.edu>
2020-06-11 16:57:22 -0500

    blm tabs

--- app/src/main/java/com/example/activism_app/BLM.java
+++ app/src/main/java/com/example/activism_app/BLM.java
@@ -1,14 +1,59 @@
 package com.example.activism_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class BLM extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TabItem blmtab1, blmtab2, blmtab3;
+    public PageAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blm);
+
+        tabLayout = findViewById(R.id.TabLayout);
+        blmtab1 = findViewById(R.id.Tab1);
+        blmtab2 = findViewById(R.id.Tab2);
+        blmtab3 = findViewById(R.id.Tab3);
+        viewPager = findViewById(R.id.viewpager);
+
+        pagerAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition() == 0) {
+                    pagerAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 1) {
+                    pagerAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 3) {
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
     }
 }
---
+++ app/src/main/java/com/example/activism_app/blmtab1.java
@@ -0,0 +1,31 @@
+package com.example.activism_app;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class blmtab1 extends Fragment {
+
+
+    public blmtab1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blmtab1, container, false);
+    }
+
+}
