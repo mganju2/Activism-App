9000726cb6f57dbb90ab7bd6d126fbfdf56c19c4
Mehak <mganju2@illinois.edu>
2020-06-24 15:14:37 -0500

    toolbar on main page

--- app/src/main/java/com/example/activism_app/MainActivity.java
+++ app/src/main/java/com/example/activism_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.activism_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("The Activist");
 
         ImageButton blm = findViewById(R.id.BLM);
         blm.setOnClickListener(new View.OnClickListener(){
