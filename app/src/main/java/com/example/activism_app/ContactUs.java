e027c6a8093d260d72b5776cc8e803d0c4bb44e3
mehakganju <mganju2@illinois.edu>
2020-07-21 12:23:53 -0500

    added toolbar to contact us section

--- app/src/main/java/com/example/activism_app/ContactUs.java
+++ app/src/main/java/com/example/activism_app/ContactUs.java
@@ -1,6 +1,7 @@
 package com.example.activism_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,10 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Contact Us");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
