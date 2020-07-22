d30d020e91cc269decdf9a74f28e7ae3f61fffc7
mehakganju <mganju2@illinois.edu>
2020-07-22 00:19:18 -0500

    attempted post request

--- app/src/main/java/com/example/activism_app/ContactUs.java
+++ app/src/main/java/com/example/activism_app/ContactUs.java
@@ -1,17 +1,24 @@
 package com.example.activism_app;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.os.Bundle;
-import android.se.omapi.Session;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
-
-import java.util.Properties;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class ContactUs extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +29,72 @@ public class ContactUs extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Contact Us");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button sendButton = findViewById(R.id.ButtonSend);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String myStr = "https://docs.google.com/forms/d/e/1FAIpQLSc71DlzyTtE7f3qxhtZvZAJtXmHRsPQKRgmkRZjOk0IjWHkYw/viewform?usp=pp_url&entry.1844187849=Ashish+Ganju&entry.1599659447=ashganju@gmail.com&entry.1709743245=test+message+123&submit=Submit";
+                URL obj = null;
+                try {
+                    obj = new URL(myStr);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                //Toast.makeText(ContactUs.this, obj.toString(), Toast.LENGTH_SHORT).show();
+                /*//System.out.println("Name = " + findViewById(R.id.EditTextName).toString());
+                String myStr = "entry.1844187849=" + findViewById(R.id.EditTextName).toString() + "&entry.1599659447=" + findViewById(R.id.EditTextEmail).toString() + "&entry.1599659447=" + findViewById(R.id.EditTextBody).toString();
+                URL obj = null;
+                String myStr ="https://docs.google.com/forms/d/e/1FAIpQLSc71DlzyTtE7f3qxhtZvZAJtXmHRsPQKRgmkRZjOk0IjWHkYw/viewform?usp=pp_url&entry.1844187849=Ashish+Ganju&entry.1599659447=ashganju@gmail.com&entry.1709743245=test+message+123&submit=Submit";
+                //System.out.println("Name = " + findViewById(R.id.EditTextName).toString());
+                try {
+                    //obj = new URL("https://docs.google.com/forms/d/e/1FAIpQLSc71DlzyTtE7f3qxhtZvZAJtXmHRsPQKRgmkRZjOk0IjWHkYw/formResponse?" + myStr + "&submit=Submit");
+                    obj = new URL(myStr);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }*/
+                HttpURLConnection con = null;
+                try {
+                    con = (HttpURLConnection) obj.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    con.setRequestMethod("POST");
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                }
+                //con.setRequestProperty("User-Agent", "Mozilla/5.0");
+                con.setDoOutput(true);
+                /*OutputStream os = null;
+                try {
+                    os = con.getOutputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    os.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+                int responseCode = 0;
+                try {
+                    responseCode = con.getResponseCode();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (responseCode == HttpURLConnection.HTTP_OK) { //success
+                    Toast.makeText(ContactUs.this, "SUCCESS", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ContactUs.this, "FAILED", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-    public void sendEmail(View button) {
-        // Do click handling here
-    }
-
-    final EditText nameField = (EditText) findViewById(R.id.EditTextName);
-    String name = nameField.getText().toString();
-
-    final EditText emailField = (EditText) findViewById(R.id.EditTextEmail);
-    String email = emailField.getText().toString();
-
-    final EditText feedbackField = (EditText) findViewById(R.id.EditTextFeedbackBody);
-    String feedback = feedbackField.getText().toString();
-
-    final CheckBox responseCheckbox = (CheckBox) findViewById(R.id.CheckBoxResponse);
-    boolean bRequiresResponse = responseCheckbox.isChecked();
 
 }
