801363ab3a2d0ffa0193fc1e4bb5226d819e8d09
mehakganju <mganju2@illinois.edu>
2020-07-26 17:24:57 -0500

    metoo hyperlinks

--- app/src/main/java/com/example/activism_app/metab2.java
+++ app/src/main/java/com/example/activism_app/metab2.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 
 /**
@@ -24,8 +28,45 @@ public class metab2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_metab2, container, false);
+
+        TextView myDonation1 = myView.findViewById(R.id.meDonation1);
+        myDonation1.setText("Me Too Donations");
+        Pattern pattern1 = Pattern.compile("");
+        String scheme1 = "https://metoomvmt.org/donate/";
+        Linkify.addLinks(myDonation1, pattern1, scheme1);
+
+        TextView myDonation2 = myView.findViewById(R.id.meDonation2);
+        myDonation1.setText("National Sexual Violence Resource Center");
+        Pattern pattern2 = Pattern.compile("");
+        String scheme2 = "https://www.nsvrc.org/donate";
+        Linkify.addLinks(myDonation2, pattern2, scheme2);
+
+        TextView myDonation3 = myView.findViewById(R.id.meDonation3);
+        myDonation1.setText("Rape, Abuse, and Incest National Network (RAINN)");
+        Pattern pattern3 = Pattern.compile("");
+        String scheme3 = "https://www.rainn.org/donate";
+        Linkify.addLinks(myDonation3, pattern3, scheme3);
+
+        TextView myDonation4 = myView.findViewById(R.id.meDonation4);
+        myDonation1.setText("End Rape On Campus (EROC)");
+        Pattern pattern4 = Pattern.compile("");
+        String scheme4 = "https://endrapeoncampus.org/our-team-1/";
+        Linkify.addLinks(myDonation4, pattern4, scheme4);
+
+        TextView myDonation5 = myView.findViewById(R.id.meDonation5);
+        myDonation1.setText("Time’s Up");
+        Pattern pattern5 = Pattern.compile("");
+        String scheme5 = "https://timesupnow.org/";
+        Linkify.addLinks(myDonation5, pattern5, scheme5);
+
+        TextView myPetition1 = myView.findViewById(R.id.mePetition1);
+        myDonation1.setText("Me Too Petitions");
+        Pattern pattern6 = Pattern.compile("");
+        String scheme6 = "https://www.change.org/m/metoo";
+        Linkify.addLinks(myPetition1, pattern6, scheme6);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_metab2, container, false);
+        return myView;
     }
 
 }
--- app/src/main/java/com/example/activism_app/metab3.java
+++ app/src/main/java/com/example/activism_app/metab3.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 
 /**
@@ -24,8 +28,45 @@ public class metab3 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_metab3, container, false);
+
+        TextView myInfo1 = myView.findViewById(R.id.meInfo1);
+        myInfo1.setText("How Tarana Burke started MeToo");
+        Pattern pattern1 = Pattern.compile("");
+        String scheme1 = "https://www.businessinsider.com/how-the-metoo-movement-started-where-its-headed-tarana-burke-time-person-of-year-women-2017-12";
+        Linkify.addLinks(myInfo1, pattern1, scheme1);
+
+        TextView myInfo2 = myView.findViewById(R.id.meInfo2);
+        myInfo2.setText("MeToo over a Decade");
+        Pattern pattern2 = Pattern.compile("");
+        String scheme2 = "https://www.nbcnews.com/think/opinion/me-too-movement-s-success-took-decade-work-not-just-ncna1108206";
+        Linkify.addLinks(myInfo2, pattern2, scheme2);
+
+        TextView myInfo3 = myView.findViewById(R.id.meInfo3);
+        myInfo2.setText("2018 the year of MeToo");
+        Pattern pattern3 = Pattern.compile("");
+        String scheme3 = "https://www.cnn.com/videos/us/2018/12/23/me-too-2018-wrap-lc-me-cs-orig.cnn";
+        Linkify.addLinks(myInfo3, pattern3, scheme3);
+
+        TextView myInfo4 = myView.findViewById(R.id.meInfo4);
+        myInfo2.setText("How MeToo is Changing American Culture");
+        Pattern pattern4 = Pattern.compile("");
+        String scheme4 = "https://www.kjrh.com/news/national-politics/the-race-2020/how-the-metoo-movement-is-changing-culture";
+        Linkify.addLinks(myInfo4, pattern4, scheme4);
+
+        TextView myInfo5 = myView.findViewById(R.id.meInfo5);
+        myInfo2.setText("How it's Changing the Modern World");
+        Pattern pattern5 = Pattern.compile("");
+        String scheme5 = "https://www.youtube.com/watch?v=ATYK2svJ6eM";
+        Linkify.addLinks(myInfo5, pattern5, scheme5);
+
+        TextView myInfo6 = myView.findViewById(R.id.meInfo6);
+        myInfo2.setText("NSVRC Podcast");
+        Pattern pattern6 = Pattern.compile("");
+        String scheme6 = "https://www.nsvrc.org/podcasts";
+        Linkify.addLinks(myInfo6, pattern6, scheme6);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_metab3, container, false);
+        return myView;
 
     }
 }
