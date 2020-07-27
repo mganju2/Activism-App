47801d4dd151868e0a815360b22a3327167c2f54
mehakganju <mganju2@illinois.edu>
2020-07-26 19:06:30 -0500

    pride hyperlinks

--- app/src/main/java/com/example/activism_app/pridetab2.java
+++ app/src/main/java/com/example/activism_app/pridetab2.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 
 /**
@@ -24,8 +28,52 @@ public class pridetab2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_pridetab2, container, false);
+
+        TextView myDonation1 = myView.findViewById(R.id.pDonation1);
+        myDonation1.setText("The Trevor Project");
+        Pattern pattern1 = Pattern.compile("");
+        String scheme1 = "https://www.thetrevorproject.org/";
+        Linkify.addLinks(myDonation1, pattern1, scheme1);
+
+        TextView myDonation2 = myView.findViewById(R.id.pDonation2);
+        myDonation1.setText("Point Foundation");
+        Pattern pattern2 = Pattern.compile("");
+        String scheme2 = "https://pointfoundation.org/";
+        Linkify.addLinks(myDonation2, pattern2, scheme2);
+
+        TextView myDonation3 = myView.findViewById(R.id.pDonation3);
+        myDonation1.setText("National Center for Transgender Equality");
+        Pattern pattern3 = Pattern.compile("");
+        String scheme3 = "https://transequality.org/";
+        Linkify.addLinks(myDonation3, pattern3, scheme3);
+
+        TextView myDonation4 = myView.findViewById(R.id.pDonation4);
+        myDonation1.setText("GLSEN");
+        Pattern pattern4 = Pattern.compile("");
+        String scheme4 = "https://www.glsen.org/";
+        Linkify.addLinks(myDonation4, pattern4, scheme4);
+
+        TextView myDonation5 = myView.findViewById(R.id.pDonation5);
+        myDonation1.setText("It Gets Better");
+        Pattern pattern5 = Pattern.compile("");
+        String scheme5 = "https://itgetsbetter.org/";
+        Linkify.addLinks(myDonation5, pattern5, scheme5);
+
+        TextView myPetition1 = myView.findViewById(R.id.pPetition1);
+        myDonation1.setText("LGBTQIA+ Petitions");
+        Pattern pattern6 = Pattern.compile("");
+        String scheme6 = "https://www.thepetitionsite.com/lgbtq-rights/#hottest";
+        Linkify.addLinks(myPetition1, pattern6, scheme6);
+
+        TextView myPetition2 = myView.findViewById(R.id.pPetition2);
+        myDonation1.setText("Change.org - LGBTQIA+ Petitions");
+        Pattern pattern7 = Pattern.compile("");
+        String scheme7 = "https://www.change.org/t/lgbt-rights-3";
+        Linkify.addLinks(myPetition2, pattern7, scheme7);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pridetab2, container, false);
+        return myView;
     }
 
 }
